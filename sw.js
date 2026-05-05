088a913b5ae183c964022eb7cfb5719fe9b7999c
nachumrubin <nachum.a.rubin@gmail.com>
2026-05-05 13:11:24 +0300

    Fix online renderBoard opRep reference error

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.addEventListener('notificationclick', function(e) {
   );
 });
 
-var CACHE_NAME = 'boost-20260505120000';
+var CACHE_NAME = 'boost-20260505100249';
 var ASSETS = [
   './',
   './index.html',
