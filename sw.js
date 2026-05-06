a2237a8d5172a10db6f34d0f4d2d0eba43f7280e
nachumrubin <nachum.a.rubin@gmail.com>
2026-05-06 09:11:02 +0300

    Fix notification settings button

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.addEventListener('notificationclick', function(e) {
   );
 });
 
-var CACHE_NAME = 'boost-20260506055000';
+var CACHE_NAME = 'boost-20260506060220';
 var ASSETS = [
   './',
   './index.html',
